b732c4afe3b0d51b9637ee4ce0bfb27de5247557
Junrey Tejas <junreytejas@gmail.com>
2020-11-21 00:34:07 +0800

    setting prod-dev http protocol switching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        URL::forceScheme('https');
+
+        if(config('app.env') === 'production') {
+           \URL::forceScheme('https');
+        }
          Schema::defaultStringLength(191);
     }
 }
