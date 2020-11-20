eb9c33e27a68a4f02a63d20b9e1874e758379f24
Junrey Tejas <junreytejas@gmail.com>
2020-11-21 01:40:35 +0800

    web routes update

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
+Route::get('/events', [EventsController::class, 'index']);
+Route::post('/events/create', [EventsController::class, 'create']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
